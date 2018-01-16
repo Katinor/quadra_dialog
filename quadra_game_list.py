af0c62c4be12e41d4597e655221e4d94dbebccec
Katinor <katinor@4ears.net>
2018-01-16 21:51:40 +0900

    Add files via upload

---
+++ quadra_game_list.py
@@ -0,0 +1,11 @@
+steam_shop = 'http://store.steampowered.com/app/'
+
+game_number = {
+	"playerunknown's battlegrounds" : ['어쭈. 치킨 한번 먹어봤나보네?','578080'],
+	'darkest dungeon' : ['정말 괜찮은 게임이지. 오늘도 의지를 시험해보자구!','262060']
+}
+
+game_synonym = {
+	"playerunknown's battlegrounds" : ['배그','배틀그라운드'],
+	'darkest dungeon' : ['다키스트 던전','다키스트던전','닼던']
+}
