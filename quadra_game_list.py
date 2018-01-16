0b62779cf9799e29b94a3c5da5517be34e94c011
Katinor <katinor@librewiki.net>
2018-01-16 23:50:00 +0900

    asdf
    asdf

--- quadra_game_list.py
+++ quadra_game_list.py
@@ -2,30 +2,30 @@ steam_shop = 'http://store.steampowered.com/app/'
 
 game_number = {
 	"playerunknown's battlegrounds" : ['어쭈. 치킨 한번 먹어봤나보네?','578080'],
-	'darkest dungeon' : ['정말 괜찮은 게임이지. 오늘도 의지를 시험해보자구!','262060']
-	'ftl: faster than light' : ['제발 버레II 나와라..','212680']
-	'the binding of issac: rebirth' : ['홀리 멘틀 개사기','250900']
-	'downwell' : ['에라이 카레존망겜','360740']
-	'enter the gungeon' : ['예에에에에~ 엔터 더 건-전'. '311690']
-	'titan souls' : ['아- 아- 아- (따라부르기)','297130']
-	'you must build a boat' : ['유! 머스트! 빌드! 어! 보트!','290890']
-	'doki doki literature club' : ['그래.. 참 두근두근한 게임이지.. (시선회피), '698780']
-        'paper, please' : ['동무 려권내라우!', '239030']
-        'reigns' : ['왕위를 계승중입니다, 아버지. - 워크래프트'.'474750']
-	'this war of mine' : ['그거 분위기는 우울한데 실상은 살인 난장판인 게임인 거로 아는데..'.'282070']
-
+	'darkest dungeon' : ['정말 괜찮은 게임이지. 오늘도 의지를 시험해보자구!','262060'],
+	'ftl: faster than light' : ['단 하나의 미사일로도, 무너질 수 있는법이야.','212680'],
+	'the binding of issac: rebirth' : ['조금은 기괴하지만, 나름 쏘는 맛은 있는 게임이지.','250900'],
+	'downwell' : [0,'360740'],
+	'enter the gungeon' : ['그저 피하고 쏘면 돼. 그럼 엔-터 더 건전!','311690'],
+	'titan souls' : ['기회는 단 한번인데, 그 기회도 스스로 쟁취해야 할거야.','297130'],
+	'you must build a boat' : [0,'290890'],
+	'doki doki literature club' : ['그래.. 참 두근두근한 게임이지.. (시선회피)','698780'],
+	'paper, please' : ['그저 여권만 처리하는 게임이라면 얼마나 좋을까.','239030'],
+	'reigns' : ['너의 선택이 너를 죽일수도, 나라를 살릴수도 있어.','474750'],
+	'this war of mine' : ['전쟁. 전쟁은 변하지 않는, 최악의 사건이지.','282070']
+}
 
 game_synonym = {
 	"playerunknown's battlegrounds" : ['배그','배틀그라운드'],
-	'darkest dungeon' : ['다키스트 던전','다키스트던전','닼던']
-	'ftl: faster than light' : ['ftl', 'FTL']
-	'the biding of issac: rebirth' : ['아이작의 번제 리버스', '아이작의번제 리버스'
-        'downwell' : ['다운웰']
-	'enter the gungeon' : ['엔터 더 건전'. '엔터더건전', '건전']
-	'titan souls' : ['타이탄 소울즈', '타이탄소울즈']
-	'you must build a boat' : ['유 머스트 빌드 어 보트']
-	'doki doki literature club' : ['두근두근 문예부', '두근두근문예부', '문예부']
-	'paper please' : ['페이퍼 플리즈', '페이퍼플리즈', '동무 려권내라우', '동무려권내라우']
-	'reigns' : ['레인즈']
+	'darkest dungeon' : ['다키스트 던전','다키스트던전','닼던'],
+	'ftl: faster than light' : ['ftl'],
+	'the biding of issac: rebirth' : ['아이작의 번제 리버스', '아이작의번제 리버스']
+	'downwell' : ['다운웰'],
+	'enter the gungeon' : ['엔터 더 건전'. '엔터더건전', '건전'],
+	'titan souls' : ['타이탄 소울즈', '타이탄소울즈'],
+	'you must build a boat' : ['유 머스트 빌드 어 보트'],
+	'doki doki literature club' : ['두근두근 문예부', '두근두근문예부', '문예부'],
+	'paper please' : ['페이퍼 플리즈', '페이퍼플리즈', '동무 려권내라우', '동무려권내라우'],
+	'reigns' : ['레인즈'],
 	'this war of mine' : ['디스 워 오브 마인', '디스워오브마인']
 }
