77bec8eac20d67672621cd6d6ca639be3f5a3cbf
Katinor <katinor@librewiki.net>
2018-02-13 14:00:36 +0900

    wow!
    good

---
+++ quadra_love.py
@@ -0,0 +1,124 @@
+import os
+import random
+import quadrabot_config
+import time
+
+MAX_LOVE = 300
+MIN_LOVE = -100
+
+def enable(user_id):
+	profile_name = "love_telegram_database/"+str(user_id)+".txt"
+	if os.path.exists(profile_name):
+		return True
+	else:
+		return False
+		
+def isAdmin(user_id):
+	if user_id in quadrabot_config.ADMIN:
+		return True
+	else: return False
+
+def createNew(user_id):
+	now = time.time()
+	profile_name = "love_telegram_database/"+str(user_id)+".txt"
+	fp = open(profile_name, 'a')
+	fp.write("0\n")
+	fp.write(str(now))
+	fp.close()
+
+def mody(user_id,value,now):
+	profile_name = "love_telegram_database/"+str(user_id)+".txt"
+	fp = open(profile_name, 'w')
+	fp.write(str(value)+'\n')
+	fp.write(str(now))
+	fp.close()
+
+def check(user_id):
+	profile_name = "love_telegram_database/"+str(user_id)+".txt"
+	fp = open(profile_name,'r')
+	target = fp.readlines()
+	fp.close()
+	target[0] = int(target[0])
+	target[1] = float(target[1])
+	return target
+
+def checkUser(user_id,forward_id):
+	if isAdmin(user_id):
+		if enable(forward_id) == False: createNew(forward_id)
+		return check(forward_id)[0]
+	else: return False
+
+#switch
+#0 = lifetime
+#1 = talk
+#2 = search
+#3 = dis
+#4 = hentai
+#5 = hentai_search
+#10 = baseball
+def process(user_id,switch,ref):
+	if enable(user_id) == False: createNew(user_id)
+	data = check(user_id)
+	now = data[1]
+	proc = False
+	if switch < 3:
+		if now+2 < time.time():
+			if switch == 0:
+				now = time.time()
+				value = data[0] + 1
+				proc = True
+			if switch == 1:
+				now = time.time()
+				value = data[0] + 1
+				proc = True
+			if switch == 2:
+				now = time.time()
+				value = data[0] + 1
+				proc = True
+			if switch == 3:
+				now = time.time()
+				value = data[0] + 2
+				proc = True
+	else:
+		if switch == 10:
+			if ref > 10: ref = 10
+			temp1 = 11 - ref
+			temp1 = temp1//10 + temp1//9 + (temp1//7)*2 + (temp1//6)*3
+			inc = temp1 * (MAX_LOVE - data[0]) / (MAX_LOVE-MIN_LOVE)
+			if inc < 2: inc = 2
+			value = data[0] + inc
+			proc = True
+		if switch == 4:
+			value = data[0] - 10 - (5*MAX_LOVE - data[0]) / (MAX_LOVE-MIN_LOVE)
+			proc = True
+		if switch == 5:
+			value = data[0] - 5 - (5*MAX_LOVE - data[0]) / (MAX_LOVE-MIN_LOVE)
+			proc = True
+	if proc: 
+		if value > MAX_LOVE: value = MAX_LOVE
+		if value < MIN_LOVE: value = MIN_LOVE
+		mody(user_id,int(value),now)
+
+love_0 = ["무슨 낯짝으로 내 앞에 서있는건지 모르겠는데?","저리 가. 별로 보고싶지 않으니까.","굳이 내 앞에 서 있는 이유가 뭐야?"]
+love_1 = ["참을 인도 쓰다보면 팔이 아프단것만 알아둬.","어.. 너구나. 안녕.","글쎄.. 굳이 표정을 찡그려줘야해?"]
+love_2 = ["글쎄.. 어떨까나?","서로 알아갈 시간이 더 필요할거같아.","굳이 말하자면, 좋지도 싫지도 않은 정도?"]
+love_3 = ["조금은 더 알아가고 싶은 사람, 정도로 괜찮을까?","적어도 밉지는 않아."]
+love_4 = ["친구 정도로 생각하고 있어.","더 친해져보고 싶은사람!","음.. 문자 정도는 주고받을 수 있는 사이?"]
+love_5 = ["너 정도면 믿음직한 친구지.","친구, 오늘은 어떻게 나와 놀아줄거야?","쫑긋! 너 정도면 좋은 친구지!"]
+love_6 = ["쫑긋! ..헤헤","..부끄러워! 너무 가까이 오진 마..","친구 이상, 연인 이하의.. 그냥 좋아 네가.","넌 최고의 친구 중 한명이야!"]
+
+def checkSwitch(data):
+	if data < MIN_LOVE * 0.7:
+		return random.choice(love_0)
+	elif data < MIN_LOVE * 0.2:
+		return random.choice(love_1)
+	elif data < MAX_LOVE * 0.1:
+		return random.choice(love_2)
+	elif data < MAX_LOVE * 0.3:
+		return random.choice(love_3)
+	elif data < MAX_LOVE * 0.7:
+		return random.choice(love_4)
+	elif data < MAX_LOVE * 0.9:
+		return random.choice(love_5)
+	else:
+		return random.choice(love_6)
--- quadrabot.py
+++ quadrabot.py
@@ -14,6 +14,7 @@ import quadra_game_list
 import quadrabot_config
 import quadra_baseball
 import quadra_lifetime
+import quadra_love
 
 my_token = quadrabot_config.TOKEN_TELEGRAM
 updater = Updater(my_token)
@@ -24,7 +25,7 @@ def log_append(_chat_id, _text, _type, _subtype):
 	else : what_type = _type
 	target = "[%04d-%02d-%02d %02d:%02d:%02d] trgd [%10s] from [%15s] : %s" % (_now.tm_year, _now.tm_mon, _now.tm_mday, _now.tm_hour, _now.tm_min, _now.tm_sec, what_type,str(_chat_id),_text)	
 	fp = open("log/quadra_bot_log.txt", 'a')
-	if os.path.getsize("log/quadra_bot_log.txt") >= 10240:
+	if os.path.getsize("log/quadra_bot_log.txt") >= 102400:
 		fp.close()
 		filelist = glob.glob("log/*.*")
 		filenum = len(filelist)
@@ -43,28 +44,40 @@ def version(bot, update):
 	chat_id = update.message.chat_id
 	now = log_append(chat_id, str(update.message.text), "help",0)
 	update.message.reply_text(
-		"반가워! 나는 사잽이라고해! 지금은 0.1.1 버전이야! \n나는 이런것들을 할 수 있어!\n * 쫑긋\n * 사잽아 ~~ 찾아줘/알려줘\n   * 지원 엔진 : 구글, 네이버, 나무위키, 리브레위키, 위키백과, 구스위키, 진보위키, 백괴사전\n * 사잽아 ~ 해줘/하자/어때/사줘")
+		"반가워! 나는 사잽이라고해! 지금은 0.1.1 버전이야! \n나는 이런것들을 할 수 있어!\n * 쫑긋\n * 사잽아 ~~ 찾아줘/알려줘\n   * 지원 엔진 : 구글, 네이버, 나무위키, 리브레위키, 위키백과, 구스위키, 진보위키, 백괴사전\n * 사잽아 ~ 해줘/하자/어때/사줘\n * 사잽아 야구게임 하자\n * 사잽아 나 어때")
+	quadra_love.process(update.message.from_user.id,1,0)
 
 def lifetime(bot, update):
 	chat_id = update.message.chat_id
 	now = log_append(chat_id, str(update.message.text), "lifetime",0)
 	text = quadra_lifetime.checkSwitch(now)
 	update.message.reply_text(text)
+	quadra_love.process(update.message.from_user.id,0,0)
 
 def dialog_how(bot, update):
 	chat_id = update.message.chat_id
 	now = log_append(chat_id, str(update.message.text), "d_how",0)
 	target = re.search('^사잽아 ((?:(?! 어때).)*) 어때(\?)?', str(update.message.text))
 	target = target.groups()
-
-	if target[0] in quadra_search_vocab.dis_list: 
+	
+	if target[0] == "나":
+		data = quadra_love.check(update.message.from_user.id)
+		userpri = str(update.message.from_user.first_name)+" "+str(update.message.from_user.last_name)+" ("+str(update.message.from_user.username)+") ["+str(update.message.from_user.id)+"]"
+		now = log_append(chat_id,userpri+" : "+str(data[0]), "d_how","l")
+		text = quadra_love.checkSwitch(data[0])
+		update.message.reply_text(text)
+	elif target[0] in quadra_search_vocab.dis_list: 
 		update.message.reply_text(random.choice(quadra_dialog_list.dialog_dis_how))
+		quadra_love.process(update.message.from_user.id,3,0)
 	elif target[0] in quadra_search_vocab.adult_list:
 		update.message.reply_text(random.choice(quadra_dialog_list.dialog_hentai_how))
+		quadra_love.process(update.message.from_user.id,4,0)
 	elif target[0] in quadra_dialog_list.dialog_how_list:
 		update.message.reply_text(random.choice(quadra_dialog_list.dialog_how_list[target[0]]))
+		quadra_love.process(update.message.from_user.id,1,0)
 	else:
 		update.message.reply_text("미안. 무슨 말인지 모르겠어.")
+		quadra_love.process(update.message.from_user.id,1,0)
 
 def dialog_please(bot, update):
 	chat_id = update.message.chat_id
@@ -74,13 +87,17 @@ def dialog_please(bot, update):
 
 	if target[0] in quadra_search_vocab.dis_list: 
 		update.message.reply_text(random.choice(quadra_dialog_list.dialog_dis_please))
+		quadra_love.process(update.message.from_user.id,3,0)
 	elif target[0] in quadra_search_vocab.adult_list:
 		update.message.reply_text(random.choice(quadra_dialog_list.dialog_hentai_please))
+		quadra_love.process(update.message.from_user.id,4,0)
 	elif target[0] in quadra_dialog_list.dialog_please_list:
 		update.message.reply_text(random.choice(quadra_dialog_list.dialog_please_list[target[0]]))
+		quadra_love.process(update.message.from_user.id,1,0)
 	else:
 		update.message.reply_text("미안. 뭘 해달라는건지 모르겠어.")
-
+		quadra_love.process(update.message.from_user.id,1,0)
+	
 def dialog_do(bot, update):
 	chat_id = update.message.chat_id
 	target = re.search('^사잽아 ((?:(?! 하자).)*) 하자', str(update.message.text))
@@ -90,20 +107,25 @@ def dialog_do(bot, update):
 		now = log_append(chat_id, str(update.message.text), "bb","start1")
 		temp = quadra_baseball.start(update.message.from_user.id)
 		if temp == -1:
-			update.message.reply_text("이미 플레이중인거 같은데?")
+			update.message.reply_text("이미 플레이중인거 같은데?\n3자리 수를 생각해서 \"사잽아 ~ 맞아?\" 라고 말해줘.\n그만하고 싶다면 \"사잽아 야구게임 그만할래\"라고 말해줘.")
 		else:
 			now = log_append(chat_id,str(update.message.from_user.id)+" "+str(temp), "bb","start2")
 			update.message.reply_text("좋아! 이제 시작해보자~\n \"사잽아 ~ 맞아?\" 라고 말해줘!\n그만하고 싶다면 \"사잽아 야구게임 그만할래\"라고 말해줘!")
+			quadra_love.process(update.message.from_user.id,1,0)
 	else:
 		log_append(chat_id, str(update.message.text), "d_do",0)
 		if target[0] in quadra_search_vocab.dis_list: 
 			update.message.reply_text(random.choice(quadra_dialog_list.dialog_dis_do))
+			quadra_love.process(update.message.from_user.id,3,0)
 		elif target[0] in quadra_search_vocab.adult_list:
 			update.message.reply_text(random.choice(quadra_dialog_list.dialog_hentai_do))
+			quadra_love.process(update.message.from_user.id,4,0)
 		elif target[0] in quadra_dialog_list.dialog_do_list:
 			update.message.reply_text(random.choice(quadra_dialog_list.dialog_do_list[target[0]]))
+			quadra_love.process(update.message.from_user.id,1,0)
 		else:
 			update.message.reply_text("미안. 뭘 하자는건지 모르겠어.")
+			quadra_love.process(update.message.from_user.id,1,0)
 
 def dialog_buy(bot, update):
 	chat_id = update.message.chat_id
@@ -113,12 +135,16 @@ def dialog_buy(bot, update):
 
 	if target[0] in quadra_search_vocab.dis_list: 
 		update.message.reply_text(random.choice(quadra_dialog_list.dialog_dis_buy))
+		quadra_love.process(update.message.from_user.id,3,0)
 	elif target[0] in quadra_search_vocab.adult_list:
 		update.message.reply_text(random.choice(quadra_dialog_list.dialog_hentai_buy))
+		quadra_love.process(update.message.from_user.id,4,0)
 	elif target[0] in quadra_dialog_list.dialog_buy_list:
 		update.message.reply_text(random.choice(quadra_dialog_list.dialog_buy_list[target[0]]))
+		quadra_love.process(update.message.from_user.id,1,0)
 	else:
 		update.message.reply_text("그건 니돈으로 사는게 어때?")
+		quadra_love.process(update.message.from_user.id,1,0)
 
 def baseball_prog(bot, update):
 	chat_id = update.message.chat_id
@@ -144,10 +170,12 @@ def baseball_prog(bot, update):
 					log_append(chat_id,"answer is "+str(data[0]), "bb","p_cor")
 					update.message.reply_text("정답이야! "+str(int(data[1])+1)+"번만에 맞췄는걸!")
 					quadra_baseball.end(user_id)
+					quadra_love.process(update.message.from_user.id,10,int(data[1])+1)
 				else:
 					log_append(chat_id,str(result[0])+"-"+str(result[1])+", answer is "+str(data[0]), "bb","p_no")
 					update.message.reply_text(str(result[0])+"스트라이크 "+str(result[1])+"볼! 이번이 "+str(int(data[1])+1)+"번째야!\n그만하고 싶다면 \"사잽아 야구게임 그만할래\"라고 말해줘!")
 					quadra_baseball.lose(user_id)
+					quadra_love.process(update.message.from_user.id,1,0)
 			else:
 				log_append(chat_id,"there are duplication", "bb","p_e3")
 				update.message.reply_text("혹시 하는말인데, 각 자리수에 같은 숫자가 들어가면 안돼. 까먹은건 아니지?\n그만하고 싶다면 \"사잽아 야구게임 그만할래\"라고 말해줘!")
@@ -176,6 +204,7 @@ def dial002(bot, update):
 	chat_id = update.message.chat_id
 	now = log_append(chat_id, str(update.message.text), "dial002",0)
 	update.message.reply_text("후에엥..ㅠㅠ")
+	quadra_love.process(update.message.from_user.id,4,0)
 
 def dial003(bot, update):
 	chat_id = update.message.chat_id
@@ -187,11 +216,13 @@ def dial003(bot, update):
 		update.message.reply_text("하하! 그렇게도 볼 수 있구나..")
 	else:
 		update.message.reply_text("그거 장난치려고 일부러 그러는거지?")
+	quadra_love.process(update.message.from_user.id,1,0)
 
 def dial004(bot, update):
 	chat_id = update.message.chat_id
 	now = log_append(chat_id, str(update.message.text), "dial004",0)
 	update.message.reply_text(random.choice(quadra_dialog_list.dialog_lone_list))
+	quadra_love.process(update.message.from_user.id,3,0)
 
 def searching(bot, update):
 	chat_id = update.message.chat_id
@@ -212,14 +243,16 @@ def searching(bot, update):
 		if term_switch == 0:
 			now = log_append(chat_id, str(update.message.text), "sch", "g_e")
 			bot.send_message(chat_id=chat_id, text="미안해. 무슨 게임인지 잘 몰라서 대신 검색해줄게! (" + quadra_search_list.search_engine["스팀"] + url_encode(target[1]) + ")", parse_mode=telegram.ParseMode.MARKDOWN)
+			quadra_love.process(update.message.from_user.id,2,0)
 		else : 
-			
 			now = log_append(chat_id, str(update.message.text), "sch", "g")
 			game_target = quadra_game_list.game_number[game_real_name]
 			if game_target[0] == 0:
 				bot.send_message(chat_id=chat_id, text="혹시 이 게임 찾으려는거 맞아? (" + quadra_game_list.steam_shop+game_target[1] + ")", parse_mode=telegram.ParseMode.MARKDOWN)
+				quadra_love.process(update.message.from_user.id,2,0)
 			else :
 				bot.send_message(chat_id=chat_id, text=game_target[0]+" (" + quadra_game_list.steam_shop+game_target[1] + ")", parse_mode=telegram.ParseMode.MARKDOWN)
+				quadra_love.process(update.message.from_user.id,2,0)
 			
 	
 	elif target[0] in quadra_search_list.search_engine:
@@ -227,38 +260,47 @@ def searching(bot, update):
 			now = log_append(chat_id, str(update.message.text), "sch", "ad")
 			update.message.reply_text("(사잽이가 당신을 경멸합니다.)")
 			bot.send_message(chat_id=chat_id, text="변태.. 이런거까지 찾아줘야해? (" + quadra_search_list.search_engine[target[0]] + url_encode(target[1]) + ")", parse_mode=telegram.ParseMode.MARKDOWN)
+			quadra_love.process(update.message.from_user.id,5,0)
 		elif target[1] in quadra_search_vocab.dis_list:
 			now = log_append(chat_id, str(update.message.text), "sch", "dis")
 			bot.send_message(chat_id=chat_id, text="... 이런걸 생각중이라면 그만둬. ..내가 너랑 함께 있어줄테니까. (" + quadra_search_list.search_engine[target[0]] + url_encode(target[1]) + ")", parse_mode=telegram.ParseMode.MARKDOWN)
+			quadra_love.process(update.message.from_user.id,3,0)
 		elif target[1] in quadra_search_vocab.wonder_list:
 			now = log_append(chat_id, str(update.message.text), "sch", "won")
 			bot.send_message(chat_id=chat_id, text=quadra_search_vocab.wonder_list[target[1]] + " (" + quadra_search_list.search_engine[target[0]] + url_encode(target[1]) + ")", parse_mode=telegram.ParseMode.MARKDOWN)
+			quadra_love.process(update.message.from_user.id,2,0)
 		else:
 			now = log_append(chat_id, str(update.message.text), "sch", 0)
 			bot.send_message(chat_id=chat_id, text="이거 찾으려는거 맞지? (" + quadra_search_list.search_engine[target[0]] + url_encode(target[1]) + ")", parse_mode=telegram.ParseMode.MARKDOWN)
+			quadra_love.process(update.message.from_user.id,2,0)
 	elif target[1] in quadra_search_list.hentai_url:
 		now = log_append(chat_id, str(update.message.text), "sch", "d_ad")
 		update.message.reply_text("(사잽이가 당신을 경멸합니다.)")
 		bot.send_message(chat_id=chat_id, text="너는 정말 최악의 변태구나.. 자, 여깄어. (" + quadra_search_list.hentai_url[target[1]] + ")", parse_mode=telegram.ParseMode.MARKDOWN)
+		quadra_love.process(update.message.from_user.id,5,0)
 	elif target[1] in quadra_search_list.direct_url:
 		now = log_append(chat_id, str(update.message.text), "sch", "d")
 		bot.send_message(chat_id=chat_id, text="거기라면 나도 알고있어! 바로 보내줄께! (" + quadra_search_list.direct_url[target[1]] + ")", parse_mode=telegram.ParseMode.MARKDOWN)
+		quadra_love.process(update.message.from_user.id,2,0)
 	else:
 		if target[1] in quadra_search_vocab.adult_list:
 			now = log_append(chat_id, str(update.message.text), "sch", "n_ad")
 			update.message.reply_text("(사잽이가 당신을 경멸합니다.)")
 			bot.send_message(chat_id=chat_id, text=target[1] + "라니... 변태! 이런거까지 찾아줘야해? (" + quadra_search_list.search_engine["구글"] + url_encode(target[1]) + ")", parse_mode=telegram.ParseMode.MARKDOWN)
+			quadra_love.process(update.message.from_user.id,5,0)
 		elif target[1] in quadra_search_vocab.dis_list:
 			now = log_append(chat_id, str(update.message.text), "sch", "n_dis")
 			bot.send_message(chat_id=chat_id, text=target[1] + "같은걸 생각중이라면 그만둬. ..내가 너랑 함께 있어줄테니까. (" + quadra_search_list.search_engine['구글'] + url_encode( target[1]) + ")", parse_mode=telegram.ParseMode.MARKDOWN)
+			quadra_love.process(update.message.from_user.id,3,0)
 		elif target[1] in quadra_search_vocab.wonder_list:
 			now = log_append(chat_id, str(update.message.text), "sch", "n_won")
 			bot.send_message(chat_id=chat_id, text=quadra_search_vocab.wonder_list[target[1]] + " (" + quadra_search_list.search_engine['구글'] + url_encode(target[1]) + ")", 	 parse_mode=telegram.ParseMode.MARKDOWN)
+			quadra_love.process(update.message.from_user.id,2,0)
 		else:
 			now = log_append(chat_id, str(update.message.text), "sch", "n")
 			bot.send_message(chat_id=chat_id, text="이거 찾으려는거 맞지? (" + quadra_search_list.search_engine["구글"] + url_encode(target[1]) + ")", parse_mode=telegram.ParseMode.MARKDOWN)
-
-
+			quadra_love.process(update.message.from_user.id,2,0)
+	
 updater.dispatcher.add_handler(RegexHandler('^사잽아 (?:((?:(?!에서).)*)에서 )?((?:(?! (알려줘|찾아줘)).)*) (알려줘|찾아줘)', searching))
 
 updater.dispatcher.add_handler(RegexHandler('^사잽아 ((?:(?! (해줘|할래)).)*) (해줘|할래)', dialog_please))
@@ -271,7 +313,6 @@ updater.dispatcher.add_handler(RegexHandler('^사잽아$', lifetime))
 updater.dispatcher.add_handler(RegexHandler('^사잽아 놀아줘$', lifetime))
 
 updater.dispatcher.add_handler(RegexHandler('^사잽아 도와줘$', version))
-
 help_handler = CommandHandler('help', version)
 updater.dispatcher.add_handler(help_handler)
 
